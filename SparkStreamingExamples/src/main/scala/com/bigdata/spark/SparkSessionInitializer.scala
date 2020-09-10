783ab6eb7b3e1f22353803ea7b09f0ba4f423d02
AshokKumarChoppadandi <ashokkumar98778@gmail.com>
2020-09-10 12:03:50 +0530

    Spark Listner class modified to read Spark Configuration & Environment perperties

--- SparkStreamingExamples/src/main/scala/com/bigdata/spark/SparkSessionInitializer.scala
+++ SparkStreamingExamples/src/main/scala/com/bigdata/spark/SparkSessionInitializer.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.SparkSession
 
 object SparkSessionInitializer {
   def getSparkSession(appName: String): SparkSession = {
-    val sparkSession = SparkSession.builder().appName(appName).master("local[*]").getOrCreate()
+    val sparkSession = SparkSession.builder().appName(appName)/*.master("local[*]")*/.getOrCreate()
     sparkSession.sparkContext.setLogLevel("ERROR")
     sparkSession
   }
--- SparkStreamingExamples/src/main/scala/com/bigdata/spark/listener/CustomSparkListener.scala
+++ SparkStreamingExamples/src/main/scala/com/bigdata/spark/listener/CustomSparkListener.scala
@@ -1,13 +1,25 @@
 package com.bigdata.spark.listener
 
-import org.apache.spark.SparkConf
 import org.apache.spark.scheduler.{SparkListener, SparkListenerApplicationEnd}
+import org.apache.spark.sql.SparkSession
 
-class CustomSparkListener(conf: SparkConf) extends SparkListener {
+import scala.collection.JavaConversions._
+
+class CustomSparkListener(spark: SparkSession) extends SparkListener {
 
   override def onApplicationEnd(applicationEnd: SparkListenerApplicationEnd): Unit = {
+    val conf = spark.sparkContext.getConf
     val allProperties = conf.getAll
+    println("Conf User :: " + conf.get("user.name", "DefaultUser"))
+    println("Spark User :: " + spark.sparkContext.sparkUser)
+
     allProperties.foreach(println)
+    val systemProperties = System.getenv()
+    val keys = systemProperties.keySet()
+    for (key <- keys) {
+      println(s"${key}, " + systemProperties.getOrDefault(key, "No Value"))
+    }
     println("Job has been completed successfully....!!!!!")
   }
+
 }
